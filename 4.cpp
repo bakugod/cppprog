319ca954dfdef510ad90cabf416de69e31b0847f
bakugod <47696795+bakugod@users.noreply.github.com>
2019-03-04 19:50:56 +0300

    Rename ForSchool.cpp to 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+double max__elem(double first, double second, double third) {
+	std::vector<double> v{ first, second, third };
+	return *std::max_element(v.begin(), v.end());
+}
+
+int main(){
+	double first, second, third;
+	
+	std::cout << "enter numbers: ";
+	std::cin >> first >> second >> third;
+	std::cout << -max__elem(first, second, third);
+
+	//std::vector<double> v{ first, second, third };
+	//std::cout << -*std::max_element(v.begin(), v.end());
+}
