f894b1e0c921505f22ad0e216696605f7073e739
bakugod <47696795+bakugod@users.noreply.github.com>
2019-03-04 19:49:20 +0300

    Rename ForSchool.cpp to 1.cpp

---
+++ ForSchool/ForSchool/1.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include <iostream>
+
+long double fact(int input, int accum = 1) {
+	if (input == 0) return accum;
+	else return fact(input - 1, input*accum);
+}
+
+int main(){
+	int input;
+	std::cin >> input;
+	if (input > 30 || input < 0) std::cout << "I can't do it!!!";
+	else std::cout << fact(input);
+}
